4bd47065756f709496a57bb7b4a0a475c6bdeda2
Akerke Kaldyoraz <98937681+21B030835@users.noreply.github.com>
2022-02-25 14:26:44 +0600

    Add files via upload

---
+++ 3week/func1/8.py
@@ -0,0 +1,9 @@
+def spygame(a):
+    s = ''
+    for i in a:
+        if i == 0 or i == 7: 
+            s += str(i)
+    if '007' in s:
+        return True
+    else: return False
+print(spygame(list(map(int, input().split()))))
