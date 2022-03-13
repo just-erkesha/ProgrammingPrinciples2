f46702bb9e1b09954acbdddacd250243eea178a0
Akerke Kaldyoraz <98937681+21B030835@users.noreply.github.com>
2022-03-13 19:30:03 +0600

    Update 7.py

--- 5week/regex/7.py
+++ 5week/regex/7.py
@@ -1,6 +1,7 @@
 import re
 
-def snake_to_camel(word):
-        return ''.join(x.capitalize() or '_' for x in word.split('_'))
+def snake_to_camel(txt):
+         components = txt.split('_')
+         return components[0] + ''.join(x.capitalize() for x in components[1:])
 
 print(snake_to_camel(input()))
