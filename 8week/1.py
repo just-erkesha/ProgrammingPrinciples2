2863282b3a935ab1db28598588c0c1f49c071361
Akerke Kaldyoraz <98937681+21B030835@users.noreply.github.com>
2022-04-15 23:49:36 +0600

    Update 1.py

--- 8week/1.py
+++ 8week/1.py
@@ -10,7 +10,7 @@ pygame.init()
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
-#Creating colors
+#Writing colors
 BLUE  = (0, 0, 255)
 RED   = (255, 0, 0)
 GREEN = (0, 255, 0)
@@ -28,7 +28,7 @@ MONEY = 0
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
-
+#background photo
 background = pygame.image.load("img/AnimatedStreet.png")
 
 #Create a white screen 
@@ -37,20 +37,20 @@ DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
 
-class Enemy(pygame.sprite.Sprite):
-      def __init__(self):
-        super().__init__() 
+class Enemy(pygame.sprite.Sprite):#creating class of enemy cars
+      def __init__(self):#initialize the attributes of the class
+        super().__init__() #function allows us to avoid using the base class name explicitly
         self.image = pygame.image.load("img/Enemy.png")
-        self.rect = self.image.get_rect()
+        self.rect = self.image.get_rect()#get the rectangular area of the Surface
         self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
 
       def move(self):
         global SCORE
         self.rect.move_ip(0,SPEED)
         if (self.rect.bottom > 600):
-            SCORE += 1
+            SCORE += 1#each passed enemy car gives score
             self.rect.top = 0
-            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)#x, y positions
 
 
 class Player(pygame.sprite.Sprite):
@@ -63,7 +63,7 @@ class Player(pygame.sprite.Sprite):
     def move(self):
         pressed_keys = pygame.key.get_pressed()
         
-        if self.rect.left > 0:
+        if self.rect.left > 0:#car is moving in x axis
               if pressed_keys[K_LEFT]:
                   self.rect.move_ip(-5, 0)
         if self.rect.right < SCREEN_WIDTH:        
@@ -100,10 +100,6 @@ all_sprites.add(P1)
 all_sprites.add(E1)
 all_sprites.add(C1)
 
-def change_coin_weigth(coin):
-    size_of_coin = random.randint(10,60)
-    coin.image = pygame.transform.scale(coin.image, (size_of_coin, size_of_coin))
-    return coin
 
 #Game Loop
 while True:
@@ -123,9 +119,6 @@ while True:
 
     #Moves and Re-draws all Sprites
     for entity in all_sprites:
-        if isinstance(entity,Coins):
-            print('inside if cond for 1') 
-
         entity.move()
         DISPLAYSURF.blit(entity.image, entity.rect)
     
@@ -146,10 +139,11 @@ while True:
           pygame.quit()
           sys.exit()
 
+      #Collecting coins
     if pygame.sprite.spritecollideany(P1, coins):
         MONEY+=1
         if MONEY%3==0:
-            SPEED += 1 
+            SPEED += 1 #each third coin increases the speed
 
           
                
