0fb08aa11772e3894f415bac01df62c7e3741cbd
Akerke Kaldyoraz <98937681+21B030835@users.noreply.github.com>
2022-04-15 23:59:19 +0600

    Update 2.py

--- 8week/2.py
+++ 8week/2.py
@@ -1,15 +1,18 @@
+#Imports
 import pygame, sys
 import random, time
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))#main window
 pygame.display.set_caption("Snake")
+
+#Setting up fonts
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, (0, 0, 0))
 
 class Snake:
-    def __init__(self, x, y):
+    def __init__(self, x, y):#initializes the attributes of the class
         self.size = 1
         self.elements = [[x,y]]#[x0,y0], [x1,x2], ...
         self.radius = 10
@@ -20,25 +23,25 @@ class Snake:
         self.score = 0
         self.level=1
     
-    def draw(self):
+    def draw(self):#for each element draw a circle
         for element in self.elements:
             pygame.draw.circle(screen, (255, 0, 0), element, self.radius)
     
     def add_to_snake(self):
         self.size +=1
         self.score+=1
-        self.elements.append([0,0])
+        self.elements.append([0,0])#adding x and y position of new tail to the array
         self.is_add = False
         if self.score%5==0:
             self.speed += 5
-            self.level+=1
+            self.level+=1 #each 5 food gives new level and increased speed
 
     def move(self):
         if self.is_add:
             self.add_to_snake()
             
-        for i in range(self.size - 1, 0, -1):
-            self.elements[i][0] = self.elements[i-1][0]
+        for i in range(self.size - 1, 0, -1):#elenments of array reversed
+            self.elements[i][0] = self.elements[i-1][0]#new tail will have coordinates of last tail
             self.elements[i][1] = self.elements[i-1][1]
 
         self.elements[0][0] += self.dx#first element is x-coordinate
@@ -47,13 +50,13 @@ class Snake:
     def eat(self, foodx, foody):
         x = self.elements[0][0]
         y = self.elements[0][1]
-        if foodx <= x <=foodx + 10 and foody <= y <=foody + 10:
+        if foodx <= x <=foodx + 10 and foody <= y <=foody + 10:#head of snake meets food
             return True
         return False
 
 class Food:
     def __init__(self):
-        self.x = random.randint(0,490)
+        self.x = random.randint(0,490)#rndom positions of food
         self.y = random.randint(0,490)
 
     def gen(self):
@@ -61,12 +64,12 @@ class Food:
         self.y = random.randint(0,490)
 
     def draw(self):
-        pygame.draw.rect(screen, (0, 255, 0), (self.x, self.y, 10, 10))
+        pygame.draw.rect(screen, (0, 255, 0), (self.x, self.y, 10, 10))#draw food
 
 snake = Snake(100, 100)
 food = Food()
 running = True
-FPS = 30
+FPS = 30#setting fps
 step = 5
 
 clock = pygame.time.Clock()
@@ -90,10 +93,11 @@ while running:
                 snake.dx = 0
                 snake.dy = step
 
-    if snake.eat(food.x, food.y):
+    if snake.eat(food.x, food.y):#chaecks
         snake.is_add = True
         food.gen()
-
+        
+        #checks on borders
     if snake.elements[0][0] > 490 or snake.elements[0][0] < 10 or snake.elements[0][1] > 490 or snake.elements[0][1] < 10:
         time.sleep(1)        
         screen.fill((255, 0, 0))
@@ -107,7 +111,7 @@ while running:
 
     snake.move()
     screen.fill((0, 0, 0))
-
+    #showing on screen
     scores = font_small.render(str(snake.score), True, (225,0,0))
     screen.blit(scores, (10,10))
     lev = font_small.render("Level-"+str(snake.level), True, (255, 255, 255))
